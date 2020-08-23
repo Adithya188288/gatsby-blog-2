2ac0190d015be3da2046f29e04d181c869121128
Adithya <adithya.santhosh33@gmail.com>
2020-08-23 20:03:18 +0530

    added source file system plugin for netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,13 @@ module.exports = {
         path: `${__dirname}/src/pages`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
     `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
